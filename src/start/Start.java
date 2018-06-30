cba5f3952f3fb268942d7612e5fc18ff9cd88bbc
mirjanastanojevic9914 <mira.95.bb@gmail.com>
2018-06-30 17:47:06 +0200

    prvi komit

---
+++ src/start/Start.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package start;
+
+import forme.login.DoctorLogin;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Mira
+ */
+public class Start {
+    
+     public static void main(String[] args) {
+        JFrame fmain = new DoctorLogin();
+        fmain.setVisible(true);      
+    }
+    
+    
+}
